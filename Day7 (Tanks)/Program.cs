6c20ad2e397c2c818cc91cb8577d566f71b09599
Bolsyn <Serikagis@yandex.ru>
2021-01-03 18:58:15 +0600

    Add project files.

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MyClassLib.WordOfTank;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] USSR = new Tank[5] { new Tank("103"), new Tank("216"), new Tank("514"), new Tank("725"), new Tank("007") };
+            Tank[] Reich = new Tank[5] { new Tank("453"), new Tank("867"), new Tank("646"), new Tank("373"), new Tank("013") };
+            var winner = new bool[5];
+            for (int i =0; i< 5; i++)
+            {
+                winner[i] = USSR[i] * Reich[i];
+            }
+            foreach(var el in winner)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
---
+++ MyClassLib/WordOfTank/Tank.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace MyClassLib.WordOfTank
+{
+    public class Tank
+    {
+        private Random rand;
+        public string Name { get; set; }
+        public int AmmoLvl
+        {
+            get
+            {
+                return ammoLvl;
+            }
+        }
+        public int ArmorLvl
+        {
+            get
+            {
+                return ArmorLvl;
+            }
+        }
+        public int ManeuverLvl
+        {
+            get
+            {
+                return ManeuverLvl;
+            }
+        }
+
+        private string name;
+        private int ammoLvl;
+        private int armorLvl;
+        private int maneuverLvl;
+
+        public Tank(string name)
+        {
+            rand = new Random();
+            try
+            {
+                this.name = name;
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            ammoLvl = rand.Next() % 100;
+            armorLvl = rand.Next() % 100;
+            maneuverLvl = rand.Next() % 100;
+        }
+
+        public string GetAmmo()
+        {
+            return $"{ammoLvl}";
+        }
+        public string GetArmor()
+        {
+            return $"{armorLvl}";
+        }
+        public string GetManeuver()
+        {
+            return $"{maneuverLvl}";
+        }
+
+        public static bool operator *(Tank tank, Tank tank2)
+        {
+            bool ammo = false;
+            bool armor = false;
+            bool maneuver = false;
+            if (tank.ammoLvl > tank2.ammoLvl)
+            {
+                ammo = true;
+            }
+            if (tank.armorLvl > tank2.armorLvl)
+            {
+                armor = true;
+            }
+            if (tank.maneuverLvl > tank2.maneuverLvl)
+            {
+                maneuver = true;
+            }
+            if ((ammo && armor) || (ammo && maneuver) || (armor && maneuver) || (armor && ammo && maneuver))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
